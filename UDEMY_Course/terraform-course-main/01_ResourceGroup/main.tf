c8bf003e25c3379bc000fdc5ebd53fb22135fe9b
barpet007 <bartokp007@gmail.com>
2025-04-01 16:18:01 +0200

    update azurerm provider main.tf

--- UDEMY_Course/terraform-course-main/01_ResourceGroup/main.tf
+++ UDEMY_Course/terraform-course-main/01_ResourceGroup/main.tf
@@ -1,12 +1,13 @@
 provider "azurerm" {
   features {}
+  subscription_id = "d6971119-76ac-45b7-a27c-7e15dfc13c8d"
 }
 
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.26.0"
+      version = "4.25.0"
     }
   }
 }
